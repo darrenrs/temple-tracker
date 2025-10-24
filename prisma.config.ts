a1689575ddf1827c1452de25fd74fcd8dcdfa030
darrenrs <darrenrs0515@gmail.com>
2025-10-24 15:20:57 -0400

    Expose .env in prisma config to allow schema regeneration

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'prisma/config'
+import path from "node:path";
+import { defineConfig } from "prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
+  schema: path.join("prisma", "schema.prisma"),
   migrations: {
-    seed: `tsx prisma/seed.ts`,
+    path: path.join("prisma", "migrations"),
+    seed: "tsx --env-file=.env ./prisma/seed.ts",
   },
 });
